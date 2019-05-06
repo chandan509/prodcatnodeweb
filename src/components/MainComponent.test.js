b3248bb0da83c9b2acc07970960453adee9204b8
Chandan Kumar <chandan.btech9@gmail.com>
2019-05-06 17:16:07 +0530

    added test cases

--- src/components/MainComponent.test.js
+++ src/components/MainComponent.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainComponent from './MainComponent';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -11,4 +12,10 @@ it('renders without crashing', () => {
 let data = [];
 data.push({PERSON : "Bob Jones"});
 data.push({PLACE : "Washington"});
-let jsonString = JSON.stringify(data);
+//let jsonString = JSON.stringify(data);
+
+it('Check heading', () => {
+    const wrapper = shallow(<MainComponent/>);
+    const heading = 'Copy JSON here and click submit'; 
+    expect(wrapper.contains(heading)).toEqual(true);    
+});
